a56e054ab06fe13bd8324a18226caf5d00aec132
HUGUET <hug211mire@gmail.com>
2020-08-20 11:36:36 +0200

    delete decipher : bad implementation

--- tapcode.py
+++ tapcode.py
@@ -8,40 +8,23 @@ polybe = {
     5:["V","W","X","Y","Z"],
 }
 
-def prepare_sentence():
+def prepare_sentence(sentence):
     """Prepare a sentence for the encryption"""
     accent = ["âà", "éèêë", "îï", "ô", "ûü", "ç"]
     ascii = ["A", "E", "I", "O", "U", "C"]
+    i=0
     for word in accent: # Replacing accented characters possible
         for letter in word:
             sentence = sentence.replace(letter, ascii[i])
         i += 1
     for letter in "',-;:!?.":  # Remove punctuation
         sentence = sentence.replace(letter, "")
-    
+    print(sentence.upper())
     return sentence.upper()
 
-
-def decypher(sentence):
-    
-    letter = [letter for letter in sentence]
-    decypheredText = [""]*(len(sentence)//2+1)
-
-    space = [index for index, element in enumerate(letter) if element == " "]
-    print(space)
-    for s in space :
-        decypheredText[s//2-1] = " "
-    print(decypheredText)
-
-    letter = [i for i in letter if i != " "]
-
-    for i in range(0,len(letter)-1,2):
-        decypheredText.insert(i, polybe[int(letter[i])][int(letter[i+1])-1])
-        print(decypheredText)
-
-    return ''.join(decypheredText)
-
 def cypher(sentence):
+
+    sentence=prepare_sentence(sentence)
     cypheredText = []
     letter = [letter for letter in sentence]
 
@@ -59,6 +42,5 @@ def cypher(sentence):
     
             
 
-print(decypher("2315313134 12451515"))
-print(cypher("HELLO BUEB BUEE"))
+print(cypher("BONJOUR COMMENT ALLEZ VOUS ETES VOUS BIEN REPOSE"))
 #print(cypher("je n aime pas les hareng"))
