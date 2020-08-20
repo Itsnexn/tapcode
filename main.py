5c14e0a831dea6bacbb8948c0ceed16ecf6a4685
HUGUET <hug211mire@gmail.com>
2020-08-20 15:59:23 +0200

    add cli

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+import argparse
+import tapcode
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("sentence",help="Sentence to cypher or decipher")
+parser.add_argument("-d", "--decode", action="count", help="decode tapcode sentence.")
+parser.add_argument("-e", "--encode", action="count", help="encode sentences to tapcode.")
+args = parser.parse_args()
+
+if args.decode :
+    print(tapcode.decipher_matrix(args.sentence," ","."))
+
+elif args.encode:
+    print(tapcode.encipher_matrix(args.sentence," ","."))
+else:
+    print("The sentence cannot be processed !")
